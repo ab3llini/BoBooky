83f8985b1ab7c7922c22d50fe13f081bc4e7090a
gianpy15 <gianpaolo.dipietro@mail.polimi.it>
2019-04-25 23:01:43 +0200

    Things...

--- other/service/AuthorService.js
+++ other/service/AuthorService.js
@@ -1,5 +1,18 @@
 'use strict';
 
+let db = require('../db/Database.js');
+
+function functionGET(func, args) {
+    return new Promise(function(resolve, reject) {
+        func.apply(null, args)
+            .then(books => {
+                resolve(books)
+            })
+            .catch(() => {
+                reject()
+            })
+    });
+}
 
 /**
  * List all the authors in the database
@@ -9,25 +22,25 @@
  * returns List
  **/
 exports.authorGET = function(offset,limit) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "image_url" : "image_url",
-  "name" : "name",
-  "description" : "description",
-  "id" : 0
-}, {
-  "image_url" : "image_url",
-  "name" : "name",
-  "description" : "description",
-  "id" : 0
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
+    return new Promise(function(resolve, reject) {
+        var examples = {};
+        examples['application/json'] = [ {
+            "image_url" : "image_url",
+            "name" : "name",
+            "description" : "description",
+            "id" : 0
+        }, {
+            "image_url" : "image_url",
+            "name" : "name",
+            "description" : "description",
+            "id" : 0
+        } ];
+        if (Object.keys(examples).length > 0) {
+            resolve(examples[Object.keys(examples)[0]]);
+        } else {
+            resolve();
+        }
+    });
 }
 
 
@@ -35,37 +48,37 @@ exports.authorGET = function(offset,limit) {
  * Delete an existing author
  * Deletes the author specified in the id
  *
- * id String 
+ * id String
  * no response value expected for this operation
  **/
 exports.authorIdDELETE = function(id) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
+    return new Promise(function(resolve, reject) {
+        resolve();
+    });
 }
 
 
 /**
  * Fetch a specific author
  *
- * id String 
+ * id String
  * returns Author
  **/
 exports.authorIdGET = function(id) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "image_url" : "image_url",
-  "name" : "name",
-  "description" : "description",
-  "id" : 0
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
+    return new Promise(function(resolve, reject) {
+        var examples = {};
+        examples['application/json'] = {
+            "image_url" : "image_url",
+            "name" : "name",
+            "description" : "description",
+            "id" : 0
+        };
+        if (Object.keys(examples).length > 0) {
+            resolve(examples[Object.keys(examples)[0]]);
+        } else {
+            resolve();
+        }
+    });
 }
 
 
@@ -73,14 +86,14 @@ exports.authorIdGET = function(id) {
  * Update an existing author
  * Updates an existing author with the informations provided
  *
- * id String 
- * body Author 
+ * id String
+ * body Author
  * no response value expected for this operation
  **/
 exports.authorIdPUT = function(id,body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
+    return new Promise(function(resolve, reject) {
+        resolve();
+    });
 }
 
 
@@ -88,76 +101,76 @@ exports.authorIdPUT = function(id,body) {
  * Add new author
  * Add a new book to the library
  *
- * body Author 
+ * body Author
  * no response value expected for this operation
  **/
 exports.authorPOST = function(body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
+    return new Promise(function(resolve, reject) {
+        resolve();
+    });
 }
 
 
 /**
  * Delete a review from a given user
  *
- * id String 
+ * id String
  * reviewID Integer The id of the review to be removed
  * no response value expected for this operation
  **/
 exports.authorReviewDELETE = function(id,reviewID) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
+    return new Promise(function(resolve, reject) {
+        resolve();
+    });
 }
 
 
 /**
  * Get reviews for a given author
  *
- * id String 
+ * id String
  * returns List
  **/
 exports.authorReviewGET = function(id) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "author" : 6,
-  "rating" : 1.46581298050294517310021547018550336360931396484375,
-  "book_author" : 5,
-  "id" : 0,
-  "title" : "title",
-  "body" : "body",
-  "timestamp" : "2000-01-23T04:56:07.000+00:00"
-}, {
-  "author" : 6,
-  "rating" : 1.46581298050294517310021547018550336360931396484375,
-  "book_author" : 5,
-  "id" : 0,
-  "title" : "title",
-  "body" : "body",
-  "timestamp" : "2000-01-23T04:56:07.000+00:00"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
+    return new Promise(function(resolve, reject) {
+        var examples = {};
+        examples['application/json'] = [ {
+            "author" : 6,
+            "rating" : 1.46581298050294517310021547018550336360931396484375,
+            "book_author" : 5,
+            "id" : 0,
+            "title" : "title",
+            "body" : "body",
+            "timestamp" : "2000-01-23T04:56:07.000+00:00"
+        }, {
+            "author" : 6,
+            "rating" : 1.46581298050294517310021547018550336360931396484375,
+            "book_author" : 5,
+            "id" : 0,
+            "title" : "title",
+            "body" : "body",
+            "timestamp" : "2000-01-23T04:56:07.000+00:00"
+        } ];
+        if (Object.keys(examples).length > 0) {
+            resolve(examples[Object.keys(examples)[0]]);
+        } else {
+            resolve();
+        }
+    });
 }
 
 
 /**
  * Add new review for the given author
  *
- * id String 
+ * id String
  * userID Integer The user that wrote the review
- * body Review 
+ * body Review
  * no response value expected for this operation
  **/
 exports.authorReviewPOST = function(id,userID,body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
+    return new Promise(function(resolve, reject) {
+        resolve();
+    });
 }
 
