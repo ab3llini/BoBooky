f1e83050a1a311dd8fa23df9aa48908b1d89ad4a
ab3llini <albertomario.bellini@gmail.com>
2019-05-20 16:00:48 +0200

    Addresses are now loaded automatically.

--- public/js/utils/template_loader.js
+++ public/js/utils/template_loader.js
@@ -1,12 +1,14 @@
 export let append = (container, template, id, map) => {
     let $ctx = $('<div>');
-    $ctx.load(template);
-    let $ans = $ctx.first();
-    $ans.attr('id', id);
-    for (let selector in map){
-        if (map.hasOwnProperty(selector)) {
-            $ans.find(selector).html(map[selector])
+    $ctx.load(template, () => {
+        let $ans = $ctx.first();
+        $ans.attr('id', id);
+        for (let selector in map){
+            if (map.hasOwnProperty(selector)) {
+                let $o = $ans.find(selector);
+                $o.html(map[selector])
+            }
         }
-    }
-    $(container).append($ans);
+        $(container).append($ans);
+    });
 };
--- public/profile/account/address/address.js
+++ public/profile/account/address/address.js
@@ -1,10 +1,25 @@
 import * as api from '/js/utils/api.js';
-import * as modals from '/public/components/modal/modal.js'
+import * as modals from '/components/modal/modal.js'
+import * as loader from '/js/utils/template_loader.js'
 
 $(() => {
     api.get.address()
-        .then(result => {
-            console.log(result)
+        .then(addresses => {
+            addresses.forEach(function (address) {
+                loader.append(
+                    '#address-container',
+                    '/components/address/address.html',
+                    address.id,
+                    {
+                        '.name' : address.name,
+                        '.address_line_1' : address.address_line_1,
+                        '.address_line_2' : address.address_line_2,
+                        '.cap' : address.cap,
+                        '.city' : address.city,
+                        '.country' : address.country
+                    }
+                )
+            })
         })
         .catch(error => {
             modals.error(error)
