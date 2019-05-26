84a9496dfed87a0eba697222f2a647e41cd8e4b0
ab3llini <albertomario.bellini@gmail.com>
2019-05-26 19:04:25 +0200

    Working on book page + new loader, carousel, general libs & lots more

--- public/index.js
+++ public/index.js
@@ -7,22 +7,47 @@ let config = {
 
 $(() => {
 
-    // Inject carousel
-    loader.append('.whatsnew', '/components/carousel/carousel_container.html', 'whatsnew-carousel')
+    // Inject
+    loader.append('.whatsnew', '/components/carousel/container.html', 'whatsnew-carousel')
         .then(() => {
 
             api.get.books(0, 20).then(books => {
                 books.forEach(book => {
-                    loader.append_map('#whatsnew-carousel .MS-content', '/components/carousel/carousel_item.html', undefined, (o) => {
-                        o.html(book.id)
+                    loader.append_map('#whatsnew-carousel .MS-content', '/components/carousel/items/book.html', book.id, (o) => {
+                        o.find('.image').css("background-image", "url(" + book.image_href + ")");
+                        o.find('.title').html(book.title)
                     })
                         .then(() => {
                             if (book === $(books).get(-1)) {
-                                $('#whatsnew-carousel').multislider();
+                                $('#whatsnew-carousel').multislider({
+                                    interval : 3000,
+                                    hoverPause : true
+                                });
                             }
                         })
                 })
+            })
+        })
 
+    // Inject
+    loader.append('.favourites', '/components/carousel/container.html', 'favourites-carousel')
+        .then(() => {
+
+            api.get.books(100, 20).then(books => {
+                books.forEach(book => {
+                    loader.append_map('#favourites-carousel .MS-content', '/components/carousel/items/book.html', book.id, (o) => {
+                        o.find('.image').css("background-image", "url(" + book.image_href + ")");
+                        o.find('.title').html(book.title)
+                    })
+                        .then(() => {
+                            if (book === $(books).get(-1)) {
+                                $('#favourites-carousel').multislider({
+                                    interval : 3000,
+                                    hoverPause : true
+                                });
+                            }
+                        })
+                })
             })
         })
 
