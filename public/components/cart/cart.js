d51a7bebd19d9516c0ae33d46956088dabff0677
gianpy15 <gianpaolo.dipietro@mail.polimi.it>
2019-05-30 14:24:40 +0200

    Working on shopping chart

--- public/components/cart/cart.js
+++
@@ -1,8 +0,0 @@
-import * as loader from '/lib/js/utils/template_loader.js'
-
-export function load_cart() {
-    for (let i = 0; i < 10; i++) {
-        loader.append_map('.cart > .objects', '/components/cart/element.html', i, (o) => {
-        })
-    }
-}
---
+++ public/profile/cart/cart.js
@@ -0,0 +1,5 @@
+import * as loader from '/lib/js/utils/template_loader.js'
+
+$(()=> {
+    loader.append('.shopping-cart-container', '/components/cart/cart-element.html', '1')
+})
