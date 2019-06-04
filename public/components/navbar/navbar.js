6665b18afd1eb0fac6e4827e18a53ddf1bf3c7d1
ab3llini <albertomario.bellini@gmail.com>
2019-06-04 21:25:46 +0200

    Working on author.

--- public/components/navbar/navbar.js
+++ public/components/navbar/navbar.js
@@ -5,20 +5,13 @@ $(function () {
 
     // Inject navbar
     $(".nav-placeholder").load("/components/navbar/navbar.html", function() {
-        // Toggle bg color on scroll
-        /*$(document).scroll(function () {
-            let $nav = $(".navbar");
-            let didScroll = $(this).scrollTop() > $nav.height();
-            $nav.toggleClass('scrolled', didScroll);
-            $nav.toggleClass('unscrolled', !didScroll);
-            $nav.toggleClass('bg-dark', didScroll);
-        }); */
 
         // Bind collapse
         let profile_button = $('.navbar #profile-button');
         if (session.isLoggedIn()) {
             profile_button.attr('data-target', '#profile-ribbon');
             profile_button.attr('aria-controls', 'profile-ribbon')
+
         }
         else {
             profile_button.attr('data-target', '#register-login-ribbon');
