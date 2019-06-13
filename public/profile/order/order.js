fb5453df97e844e8535710d6f7a7b75ebf4e270d
gianpy15 <gianpaolo.dipietro@mail.polimi.it>
2019-06-13 18:42:30 +0200

    Fix style orders

--- public/profile/order/order.js
+++ public/profile/order/order.js
@@ -5,9 +5,9 @@ $(() => {
     let id = 'funny_id_0';
     loader.append_map('.order-container', '/components/order/order-element.html', id, (o) => {
         loader.append_map('#funny_id_0 .book-element-container', '/components/order/order-book-element.html', 0, o => {
-            o.addClass('border-top')
         });
         loader.append_map('#funny_id_0 .book-element-container', '/components/order/order-book-element.html', 1, o => {
+            o.addClass('border-top')
         })
     })
 })
